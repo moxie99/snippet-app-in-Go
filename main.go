980671490b912a55c2f495544d714b37c7cfbd89
Adeolu Oluwasegun <adeolusegun1000@gmail.com>
2026-03-12 23:23:42 +0100

    added getting snippets by id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -15,28 +17,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snppet"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Add a createSnippet handler function.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
-		// Set a new cache-control header. If an existing Cache-Control header exists 
-// it will be overwritten. 
-w.Header().Set( Cache-Control, public, max-age=31536000) 
-// In contrast, the Add() method appends a new Cache-Control header and can 
-// be called multiple times. 
-w.Header().Add( Cache-Control, public) 
-w.Header().Add( Cache-Control, max-age=31536000) 
-// Delete all values for the Cache-Control header. 
-w.Header().Del( Cache-Control) 
-// Retrieve the first value for the Cache-Control header. 
-w.Header().Get( Cache-Control)
+
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
-	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
